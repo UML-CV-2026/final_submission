1c0598a8ed12660f91eeb09bbcae0789f69e939f
yyualvin <136134023+yyualvin@users.noreply.github.com>
2026-05-01 01:53:40 +0000

    feat: reproducible training functions

--- main.py
+++
@@ -1,8 +0,0 @@
-from fundus_dataset import AugmentPair, FundusVesselDataset, CenterCropPair
-from torch.utils.data import DataLoader
-import torch.nn as nn
-import torch
-import matplotlib.pyplot as plt
-from torch.utils.data import Subset
-from torch.utils.data import random_split
-from monai.networks.nets import UNet
