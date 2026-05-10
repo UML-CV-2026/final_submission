510fea943910b918d02e03072b8e6940d1ca7f71
yyualvin <136134023+yyualvin@users.noreply.github.com>
2026-05-10 02:51:55 +0000

    chore: delete symlink and add getting dataset script

---
+++ getdataset.sh
@@ -0,0 +1,3 @@
+curl -L -o fundus.zip https://www.kaggle.com/api/v1/datasets/download/nikitamanaenkov/fundus-image-dataset-for-vessel-segmentation && \
+mkdir fundus && unzip fundus.zip -d fundus && \
+rm fundus.zip
